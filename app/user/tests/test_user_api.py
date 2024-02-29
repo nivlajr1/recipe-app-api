1acac6845f04496d942226ce0b286ccde4411575
Alvin Jackson <nivlajr1@yahoo.com>
2024-02-29 15:45:46 -0500

    Recipe Detail API v1

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -62,16 +62,16 @@ class PublicUserApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
-            email=payload['email'],
+            email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
 
     def test_create_token_for_user(self):
-        """Test generates token for valid cridentials."""
+        """Test generates token for valid credentials."""
         user_details = {
-            'name':'Test Name',
-            'email':'test@example.com',
-            'password':'test-user-password123',
+            'name': 'Test Name',
+            'email': 'test@example.com',
+            'password': 'test-user-password123',
         }
         create_user(**user_details)
 
